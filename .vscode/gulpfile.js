612e4b7b5a389052b2cbe8bd91a962f0d55d4eeb
pks9862728888 <pks9862728888@gmail.com>
2024-03-10 15:51:09 +0530

    Added gulp task to automate workflows

---
+++ .vscode/gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+// Define the input and output directories
+const inputScssDir = '../src/styles/*.scss';
+const outputCssDir = '../dist/css';
+const inputHtmlDir = '../src/html/*.html';
+const outputHtmlDir = '../dist/';
+
+// Define a Gulp task to compile SCSS to CSS
+gulp.task('sass', function() {
+  return gulp.src(inputScssDir)
+    .pipe(plumber(function(error) {
+      console.error('Error:', error.message);
+      this.emit('end');
+    }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(outputCssDir))
+    .on('end', function() {
+      console.log('SCSS compilation completed successfully. Output files:', outputCssDir);
+    });
+});
+
+// Defile a Gulp task to copy html files to dist dir
+gulp.task('htmlcopier', function() {
+  return gulp.src(inputHtmlDir)
+    .pipe(gulp.dest(outputHtmlDir))
+    .on('end', function() {
+      console.log('Html files copied to: ', outputHtmlDir);
+    });
+});
+
+// Define a Gulp task to watch SCSS files for changes
+gulp.task('watch', function() {
+  gulp.watch(inputScssDir, gulp.series('sass'));
+  gulp.watch(inputHtmlDir, gulp.series('htmlcopier'));
+});
+
+// Default task - runs 'sass' & 'htmlcopier' task first and then starts watching for changes
+gulp.task('default', gulp.series('sass', 'htmlcopier', 'watch'));
